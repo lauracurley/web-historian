f4aa81e3cbd3f426e33205c2c664b065e87327e9
lauracurley <info@lauracurley.com>
2016-03-23 19:56:23 -0700

    Moved our request-function into the request-handler function and identified the source of an error

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,84 @@
+var fs = require('fs');
+var req = require('request');
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var Promise = require('bluebird');
+
+
+// var index = require('../web/public/index.html');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  //res.end(archive.paths.list);
+
+  var serveStatic = function(req, res) {
+    var mimeTypes = {
+      '.js': 'text/javascript',
+      '.html': 'text/html',
+      '.css': 'text/css'
+    };
+    var lookup, f;
+    if (req.url === '/') { 
+      lookup = '/index.html'; 
+    } else {
+      lookup = req.url;
+    }
+
+    // var lookup = path.basename(decodeURI(req.url)) || 'index.html', f = lookup;
+    lookup = '../web/public' + lookup; f = lookup; 
+    fs.exists(f, function (exists) {
+      console.log('---------------F-------------', exists);
+      if (exists) {
+        console.log('----------EXISTS----------->', exists);
+        fs.readFile(f, 'utf8', function(err, data) {
+          if (err) {
+            res.writeHead(404); res.end('Server Error!');
+            return;
+          }
+
+          var headers = {'Content-type': mimeTypes[path.extname(req.url)]};  
+          res.writeHead(200, headers);
+          res.end(data);
+        });
+        return;
+      }
+      //doesn't exist
+      res.writeHead(404);
+      res.end();
+    });
+  };
+
+  //invoking a normal function before a asynchronous function ends?
+  serveStatic(req, res);
 };
+
+// var defaultCorsHeaders = {
+//   'access-control-allow-origin': '*',
+//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   'access-control-allow-headers': 'content-type, accept',
+//   'access-control-max-age': 10 // Seconds.
+// };
+
+// if (request.method === 'GET' && request.url === '/') {
+//   var statusCode = 200;
+//   var headers = defaultCorsHeaders;
+//   headers['Content-Type'] = 'text/html';
+//   response.writeHead(statusCode, headers);
+//   response.end('<input></input>');
+// }
+
+// var fileWriter = function(writeFilePath, profile, callback) {
+//   fs.writeFile(writeFilePath, JSON.stringify(profile), 'utf8', function(err, file) {
+//     if (err) { return callback(err, null); }
+//     callback(null, profile);
+//   });
+// };
+
+// var fileWriterAsync = Promise.promisify(fileWriter);
+
+
+
+
+
+
+
